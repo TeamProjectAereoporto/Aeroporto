0eecda720df13275542fa44e663053ffe607db1e
Salvatore Mauro <salvatoremaureo2004@gmail.com>
2025-04-15 12:32:52 +0200

    Merge remote-tracking branch 'origin/master'

--- src/main/java/model/Gate.java
+++ src/main/java/model/Gate.java
@@ -1,13 +1,25 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Gate {
     String codiceGate;
-    int voloAssegnato;
-    //il codice univoco del volo e l'attributo "volo assegnato" devono corrispondere. Dunque, quando verrà implementato il metodo per
-    //assegnare un volo ad un gate sarà importante inizializzare questo attributo con il codice univoco del volo a cui viene assegnato.
+    VoloPartenza voloAssegnato;
 
-    Gate(String coGate, int voloAss){
+    Gate(String coGate){
         codiceGate = coGate;
-        voloAssegnato = voloAss;
+        voloAssegnato = null;
+    }
+
+    public void assegnaVolo(VoloPartenza v){
+        voloAssegnato = v;
+    }
+
+    public VoloPartenza getVolo(){
+        return voloAssegnato;
+    }
+
+    public String getCodiceGate(){
+        return codiceGate;
     }
 }
