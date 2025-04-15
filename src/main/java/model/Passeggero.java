598615783fc02e2d7ca9e20d4569a58f201a69f3
LeonardiKarol <60487705+LeonardiKarol@users.noreply.github.com>
2025-04-15 14:02:47 +0200

    prenotazione, passeggero, checkin

---
+++ src/main/java/model/Passeggero.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Passeggero {
+    String nome;
+    String cognome;
+    String numeroDocumento;
+
+    Passeggero(int nome, String cognome, String numeroDocumento){
+        this.nome=nome;
+        this.cognome=cognome;
+        this.numeroDocumento=numeroDocumento;
+    }
+
+}
