c440f2feec0022102973a0c64c819a58aadb26c0
Salvatore Mauro <salvatoremaureo2004@gmail.com>
2025-05-19 13:29:21 +0200

    aggiunta icona

--- src/main/java/model/VoloArrivo.java
+++ src/main/java/model/VoloArrivo.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class VoloArrivo extends Volo{
 
-    public VoloArrivo(int codiceVolo, String compagniaAerea, Date orarioArrivo, int ritardo, statoVolo stato, String aeroportoOrigine, String gate){
+    public VoloArrivo(int codiceVolo, String compagniaAerea, String orarioArrivo, int ritardo, statoVolo stato, String aeroportoOrigine, String gate){
         super(codiceVolo,compagniaAerea,orarioArrivo,ritardo,stato,aeroportoOrigine, "Napoli", gate);
     }
 }
